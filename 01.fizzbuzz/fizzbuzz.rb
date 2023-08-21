c88f1bffa732af1967a565cdea9cfb6ff190acba
sho0021 <hakutsuslush334@gmail.com>
2023-08-21 11:18:13 +0900

    third add

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,9 +1,9 @@
 20.times do |n|
     if (n+1) % 15 == 0
         puts "fizzbuzz"
-    elsif (n+1) % 5 ==0
+    elsif (n+1) % 5 == 0
         puts "buzz"
-    elsif (n+1) % 3 ==0
+    elsif (n+1) % 3 == 0
         puts "fizz"
     else
         puts (n+1)
