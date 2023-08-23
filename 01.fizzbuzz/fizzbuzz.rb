f1fff5a72f5f48092f332df9cf3371c9a2806fc8
sho0021 <hakutsuslush334@gmail.com>
2023-08-23 10:46:56 +0900

    timesメソッドの代わりにuptoメソッドを使用。空行を削除。

--- 01.fizzbuzz/fizzbuzz.rb
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -1,12 +1,11 @@
-20.times do |n|
-    if (n+1) % 15 == 0
-        puts "fizzbuzz"
-    elsif (n+1) % 5 == 0
-        puts "buzz"
-    elsif (n+1) % 3 == 0
-        puts "fizz"
-    else
-        puts (n+1)
-    end
+1.upto(20) do |n|
+  if n % 15 == 0
+    puts "fizzbuzz"
+  elsif n % 5 == 0
+    puts "buzz"
+  elsif n % 3 == 0
+    puts "fizz"
+  else
+    puts n
+  end
 end
-
